ac41cd2a480986c6442cba01fcc6d8f1b19e05a2
Ringlings <110815694+Ringlings@users.noreply.github.com>
2022-12-18 23:35:33 +1000

    Update system information

--- src/lib/variables.ts
+++ src/lib/variables.ts
@@ -1,6 +1,6 @@
 export default {
     // Hardcoded because CLoudflare doesn't handle env vars very well
     customStrings: "false",
-    systemName: "Starchild",
-    systemId: "starc",
+    systemName: "Ringlings",
+    systemId: "vgspl",
 };
